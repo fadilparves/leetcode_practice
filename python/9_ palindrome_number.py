f25253bf2ae99f368ccc29d7b318323ac633ddeb
Mohamad Fadil <fadil.parves@gmail.com>
2022-11-10 19:32:54 +0800

    solved another easy question

---
+++ python/9_ palindrome_number.py
@@ -0,0 +1,22 @@
+""" Given an integer x, return true if x is a palindrome and false otherwise. 
+    palindrome -> An integer is a palindrome when it reads the same forward and backward. For example, 121 is a palindrome while 123 is not.
+    
+    Example:
+    Input: x = 121
+    Output: true
+    Explanation: 121 reads as 121 from left to right and from right to left.
+    
+    Example:
+    Input: x = -121
+    Output: false
+    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+    Example:
+    Input: x = 10
+    Output: false
+    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+    Constraints:
+    
+    -231 <= x <= 231 - 1
+    """

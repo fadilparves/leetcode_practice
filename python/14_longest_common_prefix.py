a5871e99576dd1725bc06ab602d195521b7bcec7
Mohamad Fadil <fadil.parves@gmail.com>
2022-11-11 15:50:28 +0800

    Not easy but doable

--- python/14_longest_common_prefix.py
+++ python/14_longest_common_prefix.py
@@ -18,3 +18,33 @@
     strs[i] consists of only lowercase English letters.
 """
 
+class ExerciseFourteen:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if not strs:
+            return ""
+        
+        # Get the shortest word in the string array
+        shortest = min(strs, key=len) 
+        # Loop the shortest word
+        for i, ch in enumerate(shortest): 
+            # Loop all the words from the string array
+            for other in strs:
+                # Check if the other words alphabet at the given index i is the same as the shortest at index i
+                if other[i] != ch:
+                    # If not the same then just return the shortest word to the given index with the same alphabet 
+                    return shortest[:i]
+
+        return shortest
+
+case1 = ["flower","flow","flight"] # Answer should be "fl"
+case2 = ["dog","racecar","car"] # Answer should be ""
+case3 = ["flower","flow","flowed","flowers"] # Answer should be "flow"
+case4 = ["flower","car","cart"] # Answer should be ""
+
+E14 = ExerciseFourteen()
+out1 = E14.longestCommonPrefix(case1)
+out2 = E14.longestCommonPrefix(case2)
+out3 = E14.longestCommonPrefix(case3)
+out4 = E14.longestCommonPrefix(case4)
+
+print(out1, out2, out3, out4) # fl, "", flow, ""

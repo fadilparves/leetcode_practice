5141f584d6ae24c634fece2a9f11b1d3a079de97
Mohamad Fadil <fadil.parves@gmail.com>
2022-11-17 21:59:45 +0800

    done and working

--- python/69_sqrt_x_.py
+++ python/69_sqrt_x_.py
@@ -1,7 +1,7 @@
 """
 Question:
 Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
-The returned integer should be non-negative as well.
+The returned integer should be non-negative as well. (Easy)
 
 You must not use any built-in exponent function or operator.
 

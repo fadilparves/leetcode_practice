8a734f971127e138597b3c5fb9e859037eb85ddf
Mohamad Fadil <fadil.parves@gmail.com>
2022-11-18 10:44:54 +0800

    added problem statement

--- python/278_first_bad_version.py
+++ python/278_first_bad_version.py
@@ -1,5 +1,26 @@
 """
 
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example:
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+
+Example:
+Input: n = 1, bad = 1
+Output: 1
+
+Solutions:
+
 Problem: Build a program that can identify the first bad version in a list of version (basically array of int) and also replicate the action of minimizing the number of calls to API.
 
 Constraints:
